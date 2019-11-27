16572595b4a8d9cdd418d5ddd460543eee8cc4be
apinto5 <54562918+apinto5@users.noreply.github.com>
2019-11-26 19:24:32 -0500

    added alert()
    re-added alert() to see if code ran

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var map;
 
 function init(){
+    alert('code ran'):
     map = new google.maps.Map(document.getElementById('canvas'), {
     center: new google.maps.LatLng(41.833002,-87.624128),//Setting Initial Position
     zoom: 17,
