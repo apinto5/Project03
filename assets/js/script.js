c6cb65959cde187f7179f2c83986d3eda0b344ff
apinto5 <54562918+apinto5@users.noreply.github.com>
2019-11-26 19:23:32 -0500

    fixed button names

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,20 +10,20 @@ function init(){
 google.maps.event.addDomListener(window, 'load', init);
 
 
-google.maps.event.addDomListener(document.getElementById('button1'), 'click', function () {
+google.maps.event.addDomListener(document.getElementById('btn1'), 'click', function () {
 	map.setCenter(new google.maps.LatLng(42.497938, -87.956880));
 	map.setZoom(17);
 	map.setMapTypeId(google.maps.MapTypeId.SATELLITE);
 });
 
-google.maps.event.addDomListener(document.getElementById('button2'), 'click', function () {
+google.maps.event.addDomListener(document.getElementById('btn2'), 'click', function () {
     map.setCenter(new google.maps.LatLng(-50.607311, 165.971229));
     map.setZoom(15);
     map.setMapTypeId(google.maps.MapTypeId.TERRAIN);
 
 });
 
-google.maps.event.addDomListener(document.getElementById('button3'), 'click', function () {
+google.maps.event.addDomListener(document.getElementById('btn3'), 'click', function () {
     map.setCenter(new google.maps.LatLng(12.370367, 23.322272));
     map.setZoom(18);
     map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
