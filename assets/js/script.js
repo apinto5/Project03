25100930087bf3cd0873b5f6709cc94877b2f394
apinto5 <54562918+apinto5@users.noreply.github.com>
2019-12-03 20:30:16 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-var setLat;
-var setLon;
-
 function initMap() {
   var centerM = {lat: 41.833608, lng: -87.624165};
   // The map, centered at Uluru
@@ -9,15 +6,5 @@ function initMap() {
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({position: centerM, map: map});
 }
-function changeLocation(setLat, setLon){
-      map.setCenter(new google.maps.LatLng(setLat, setLong));
-}
-var inputElement = document.createElement('input');
 
-inputElement.type = "button"
-inputElement.addEventListener('click', function(){
-    alert("THIS RAN")
-    changeLocation(12.370367, 23.322272);
-});
-document.body.appendChild(inputElement);​
 
