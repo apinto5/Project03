70213bc2813b7307a39d427388181f2712623bbe
apinto5 <54562918+apinto5@users.noreply.github.com>
2019-12-03 22:55:12 -0600

    added slideshow function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 window.map = undefined;
+var start = 0;
+slideshow();
 
 function initMap() {
   var centerM = {lat: 41.833608, lng: -87.624165};
@@ -12,4 +14,18 @@ function initMap() {
 function moveToLocation(lat, lng){
   const center = new google.maps.LatLng(lat, lng);
   window.map.panTo(center);
+}
+
+
+function slideshow() {
+	var beg;
+	var grab_html_element = document.getElementsByClassName("pics");
+	for (beg = 0; beg < grab_html_element.length;beg++) {
+		grab_html_element[beg].style.display="none";
+	}
+	start++;
+	if (start > grab_html_element.length) {
+    start = 1}
+	grab_html_element[start - 1].style.display = "block";
+	setTimeout(slideshow, 3000);
 }
