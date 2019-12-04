8f280ece7d782d2db940ef48f749cc32b332e5e7
apinto5 <54562918+apinto5@users.noreply.github.com>
2019-12-03 20:10:42 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var setLon;
 function initMap() {
   var centerM = {lat: 41.833608, lng: -87.624165};
   // The map, centered at Uluru
-  var map = new google.maps.Map(
+  map = new google.maps.Map(
   document.getElementById('map'), {zoom: 15, center: centerM});
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({position: centerM, map: map});
