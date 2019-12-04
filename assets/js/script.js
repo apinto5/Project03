fb00d010149ffc4ea662156838d7eaa635eac3f7
apinto5 <54562918+apinto5@users.noreply.github.com>
2019-12-03 19:55:54 -0600

    Lines 12-16: new method of setting center

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,18 @@
 var map;
+var setLat;
+var setLon;
 
 function initMap() {
   var centerM = {lat: 41.833608, lng: -87.624165};
   // The map, centered at Uluru
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 15, center: centerM});
+  document.getElementById('map'), {zoom: 15, center: centerM});
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({position: centerM, map: map});
+  document.getElementById("btn1").addEventListener("click", changeLocation(-50.607311, 65.971229));
 }
-	google.maps.event.addDomListener(window, 'load', initMap);
-
-
-	google.maps.event.addDomListener(document.getElementById('btn1'), 'click', function () {
-		map.setCenter(new google.maps.LatLng(42.497938, -87.956880));
-		map.setZoom(17);
-		map.setMapTypeId(google.maps.MapTypeId.SATELLITE);
-	});
-
-	google.maps.event.addDomListener(document.getElementById('btn2'), 'click', function () {
-	    map.setCenter(new google.maps.LatLng(-50.607311, 165.971229));
-	    map.setZoom(15);
-	    map.setMapTypeId(google.maps.MapTypeId.SATELLITE);
-
-	});
-
-	google.maps.event.addDomListener(document.getElementById('btn3'), 'click', function () {
-	    map.setCenter(new google.maps.LatLng(12.370367, 23.322272));
-	    map.setZoom(18);
-	    map.setMapTypeId(google.maps.MapTypeId.SATELLITE);
-	});
+function changeLocation(setLat, setLon){
+      map.setCenter(new google.maps.LatLng(setLat, setLong));
 }
+
 
