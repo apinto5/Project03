b807a079bb92fbf76327284ff3f82bbc1f8467b3
apinto5 <54562918+apinto5@users.noreply.github.com>
2019-12-03 19:27:59 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var map;
 
 function initMap() {
-  var centerM = {lat: 42.497938, lng: -87.956880};
+  var centerM = {lat: 41.833608, lng: -87.624165};
   // The map, centered at Uluru
   var map = new google.maps.Map(
       document.getElementById('map'), {zoom: 15, center: centerM});
@@ -9,7 +9,7 @@ function initMap() {
   var marker = new google.maps.Marker({position: centerM, map: map});
 
 	google.maps.event.addDomListener(document.getElementById('btn1'), 'click', function () {
-		map.setCenter(new google.maps.LatLng(42.497938, -87.956880));
+		map.setCenter(new google.maps.LatLng(41.833608,-87.624165));
 		map.setZoom(17);
 		map.setMapTypeId(google.maps.MapTypeId.SATELLITE);
 	});
@@ -26,6 +26,6 @@ function initMap() {
 	    map.setZoom(18);
 	    map.setMapTypeId(google.maps.MapTypeId.SATELLITE);
 	});
-}
+	}
 }
 
